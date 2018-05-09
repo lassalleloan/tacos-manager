64ff87d47a1d944516bfdce54284c18641ce3a1c
marcoadrien <adrien.marco@heig-vd.ch>
2018-05-09 14:48:44 +0200

    trouve pas home_tacos

---
+++ Tacos/app/controllers/tacosHomeController.scala
@@ -0,0 +1,26 @@
+package controllers
+
+//import dao.{CoursesDAO, StudentsDAO}
+import javax.inject._
+import play.api.mvc._
+//import play.api.routing.JavaScriptReverseRouter
+//import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
+@Singleton
+class tacosHomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  val title = "Intergalactic TACOS Food"
+
+
+  /**
+    * Call the "about" html template.
+    */
+  def tacosHome = Action {
+    Ok(views.html.tacos_home(title))
+  }
+
+}
