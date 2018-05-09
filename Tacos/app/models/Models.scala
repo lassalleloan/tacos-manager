325295973961bca607b020f607b471d3445e36b9
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2018-05-09 17:12:09 +0200

    Models

--- Tacos/app/models/Models.scala
+++ Tacos/app/models/Models.scala
@@ -9,3 +9,34 @@ case class Student(id: Option[Long], firstName: String, lastName: String, age: I
 
 // Represent a database's course <- >student entry.
 case class CourseStudent(id: Option[Long], courseId: Long, studentId: Long)
+
+case class Person(id: Option[Long], name: String, firstName: String, phone: String, mail: String,
+                  login: String, password: String, rolePerson: Long)
+
+case class RolePerson(id: Option[Long], name: String)
+
+case class Order(id: Option[Long], dateOrder: String, hourOrder: String, price: Double, person: Long)
+
+case class Fry(id: Option[Long], name: String, portion: Int, price: Double)
+
+case class OrderFry(orderId: Long, fryId: Long)
+
+case class Drink(id: Option[Long], name: String, deciliter: Int, price: Double)
+
+case class OrderDrink(ordeId: Long, drinkId: Long)
+
+case class Tacos(id: Option[Long], name: String, price: Double)
+
+case class OrderTacos(ordeId: Long, tacosId: Long)
+
+case class Sauce(id: Option[Long], name: String, spicy: Int)
+
+case class TacosSauce(tacosId: Long, sauceId: Long)
+
+case class Meat(id: Option[Long], name: String, origin: String)
+
+case class TacosMeat(tacosId: Long, meatId: Long)
+
+case class Ingredient(id: Option[Long], name: String, origin: String)
+
+case class TacosIngredient(tacosId: Long, ingredientId: Long)
