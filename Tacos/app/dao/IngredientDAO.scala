635b7b5daf652942674994c79cb748594fdc9b82
JulienBrechet <julien.brechet@heig-vd.ch>
2018-05-16 19:12:59 +0200

    Ajout du DAO IngredientDAO.

--- Tacos/app/dao/IngredientDAO.scala
+++ Tacos/app/dao/IngredientDAO.scala
@@ -1,5 +1,29 @@
 package dao
 
-class IngredientDAO {
+import scala.concurrent.Future
+import javax.inject.{Inject, Singleton}
+import models.Ingredient
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.JdbcProfile
+import scala.concurrent.{ExecutionContext, Future}
 
+trait IngredientComponent {
+  self: HasDatabaseConfigProvider[JdbcProfile] =>
+
+  import profile.api._
+
+  // This class convert the database's "ingredient" table in a object-oriented entity: the Ingredient model.
+  class ingredientTable(tag: Tag) extends Table[Ingredient](tag, "ingredient") {
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc) // Primary key, auto-incremented
+    def name = column[String]("nom")
+    def origin = column[String]("provenance")
+
+    // Map the attributes with the model.
+    def * = (id, name, origin) <> (Ingredient.tupled, Ingredient.unapply)
+  }
 }
+
+// This class contains the object-oriented list of users and offers methods to query the data.
+// A DatabaseConfigProvider is injected through dependency injection; it provides a Slick type bundling a database and
+// driver. The class extends the user query table and loads the JDBC profile configured in the application's
+// configuration file.
