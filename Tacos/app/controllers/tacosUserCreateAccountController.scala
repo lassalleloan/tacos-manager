5756131e919a3bacee44230614e8b7a50c00d6d6
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2018-05-24 08:59:09 +0200

    SignUp corrected

--- Tacos/app/controllers/tacosUserCreateAccountController.scala
+++ Tacos/app/controllers/tacosUserCreateAccountController.scala
@@ -21,37 +21,40 @@ class tacosUserCreateAccountController @Inject()(cc: ControllerComponents, userD
   val roleUserClient = 2
 
   // Declare a case class that will be used in the new connection's form
-  case class CreationRequest(firstName: String, lastName: String, phone: String, email: String, password: String, confirmPassword: String)
+  case class SignUpRequest(firstName: String, lastName: String, phone: String, email: String, password: String, confirmPassword: String)
 
   // Create a new connection form mapping, in order to map the values of the HTML form with a Scala Form
   // Need to import "play.api.data._" and "play.api.data.Forms._"
-  def creationForm = Form(
+  def signUpForm = Form(
     mapping(
-      "firstName" -> text,
-      "lastName" -> text,
+      "firstName" -> nonEmptyText,
+      "lastName" -> nonEmptyText,
       "phone" -> text,
-      "email" -> text,
-      "password" -> text,
-      "confirmPassword" -> text
-    )(CreationRequest.apply)(CreationRequest.unapply)
+      "email" -> nonEmptyText,
+      "password" -> nonEmptyText,
+      "confirmPassword" -> nonEmptyText,
+    )(SignUpRequest.apply)(SignUpRequest.unapply)
   )
 
   def createAccount = Action.async { implicit request =>
-    creationForm.bindFromRequest.fold(
-      error => Future.successful(Ok(views.html.tacos_user_create_account(title, "Une erreur est survenue. Veuillez réessayer plus tard."))),
+    signUpForm.bindFromRequest.fold(
+      errorForm => {
+        val error = "Un ou plusieurs champs obligatoire sont vide"
+        Future.successful(Ok(views.html.tacos_user_create_account(title, error)))
+      },
       u => {
-        if (u.firstName.isEmpty || u.lastName.isEmpty || u.email.isEmpty || u.password.isEmpty || u.confirmPassword.isEmpty) {
-          Future.successful(Ok(views.html.tacos_user_create_account(title, "Certains champs sont vides.")))
-        } else if (u.password != u.confirmPassword) {
-          Future.successful(Ok(views.html.tacos_user_create_account(title, "La confirmation du mot de passe ne correspond pas au mot de passe original.")))
+        if (u.password != u.confirmPassword) {
+          val error = "La confirmation du mot de passe ne correspond pas au mot de passe original"
+          Future.successful(Ok(views.html.tacos_user_create_account(title, error)))
         } else {
           userDAO.findByEmail(u.email).map {
             case Some(_) =>
-              Ok(views.html.tacos_user_create_account(title, "L'adresse email est déjà associé à un compte."))
+              val error = "L'adresse email est déjà associé à un compte"
+              Ok(views.html.tacos_user_create_account(title, error))
             case None =>
               val user = User(None, u.firstName, u.lastName, Some(u.phone), u.email, u.password, roleUserClient)
               userDAO.insert(user)
-              Ok(views.html.tacos_user_admin_connection(title, user.firstName, user.lastName))
+              Redirect("/tacosUserAdminConnection")
           }
         }
       })
